d2c62a94cdc86d994b8a9402f30dddf1e6495bed
daar <dhkblaszyk@gmail.com>
2023-04-27 19:53:55 +0200

    implement for adults

--- src/BMICalculator.php
+++ src/BMICalculator.php
@@ -15,12 +15,59 @@
  * @version     1.0.0
  */
 
-namespace daar\BMI;
+namespace daar\bmi;
 
 class BMICalculator
 {
-    public static function test()
+
+    private $weight;
+    private $length;
+    private $gender;
+    private $age;
+    private $bmi;
+
+    public function __construct(float $weight, float $length, string $gender, float $age)
     {
-        return "Hello World!";
+        $this->weight = $weight;
+        $this->length = $length;
+        $this->gender = $gender;
+        $this->age = $age;
+
+        $this->bmi = round($weight / ($length * $length), 1);
+    }
+
+    public function calculateBMI(float $weight, float $length)
+    {
+        return $this->bmi;
+    }
+
+    private function getBMICategoryForAdults($bmi)
+    {
+        if ($bmi < 16) {
+            return 'Underweight (Severe thinness)';
+        } elseif ($bmi <= 16.9) {
+            return 'Underweight (Moderate thinness)';
+        } elseif ($bmi <= 18.4) {
+            return 'Underweight (Mild thinness)';
+        } elseif ($bmi <= 24.9) {
+            return 'Normal weight';
+        } elseif ($bmi <= 29.9) {
+            return 'Overweight (Pre-obese)';
+        } elseif ($bmi <= 34.9) {
+            return 'Obese class I';
+        } elseif ($bmi <= 39.9) {
+            return 'Obese class II';
+        } else {
+            return 'Obese class III';
+        }
+    }
+
+    public function BMICategory()
+    {
+        if ($this->age >= 20) {
+            return  $this->getBMICategoryForAdults($this->bmi);
+        } else {
+            // return $this->getBMICategoryForChildren($bmi, $age, $gender);
+        }
     }
 }
