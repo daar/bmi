d17c79d9fb6f2c35f5bc3490042f89c6ec0d3358
daar <dhkblaszyk@gmail.com>
2023-04-27 14:57:49 +0200

    initial class

---
+++ src/BMICalculator.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * BMI Calculator Library for FilamentPHP
+ *
+ * This library calculates the Body Mass Index (BMI) for a given weight and height and returns the corresponding BMI category
+ * according to the World Health Organization (WHO) criteria.
+ *
+ * References:
+ * WHO Child Growth Standards: https://www.who.int/childgrowth/standards/en/
+ * WHO BMI Classification: https://www.who.int/health-topics/obesity#tab=tab_1
+ *
+ * @author      Your Name
+ * @license     MIT
+ * @version     1.0.0
+ */
+
+namespace daar\BMI;
+
+class BMICalculator
+{
+    public static function test()
+    {
+        return "Hello World!";
+    }
+}
